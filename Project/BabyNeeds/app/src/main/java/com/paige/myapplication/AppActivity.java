81adec93617beb72bdc0358aa8d91b4b9b8939fa
shin <proscus0706@gmail.com>
2019-12-22 17:40:41 +0900

    first commit

---
+++ Project/BabyNeeds/app/src/main/java/com/paige/myapplication/AppActivity.java
@@ -0,0 +1,140 @@
+package com.paige.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.paige.myapplication.data.DatabaseHandler;
+import com.paige.myapplication.model.Item;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.List;
+
+public class AppActivity extends AppCompatActivity {
+
+    public static final String TAG = "AppAct";
+
+    private AlertDialog.Builder builder;
+    private AlertDialog dialog;
+    private Button saveButton;
+    private EditText babyItem, itemQuantity, itemColor, itemSize;
+
+    private DatabaseHandler db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        db = new DatabaseHandler(AppActivity.this);
+
+        //check if item was saved
+        List<Item> items = db.getAllItems();
+        for(Item item : items){
+            Log.d(TAG, "onCreate: " + item.getItemName());
+        }
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                createPopupDialog();
+            }
+
+
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void saveItem(View view){
+        //Todo: save each baby item to database
+        Item item = new Item();
+        String newItem = babyItem.getText().toString().trim();
+        String newColor = itemColor.getText().toString().trim();
+        int quantity = Integer.parseInt(itemQuantity.getText().toString().trim());
+        int size = Integer.parseInt(itemSize.getText().toString().trim());
+
+        item.setItemName(newItem);
+        item.setItemColor(newColor);
+        item.setItemQuantity(quantity);
+        item.setItemSize(size);
+
+        db.addItem(item);
+
+        Snackbar.make(view, "Item Saved", Snackbar.LENGTH_SHORT).show();
+
+        //Todo: move to next screen - details screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //code to be run
+                dialog.dismiss();
+                //Todo: move to next screen - details screen
+                startActivity(new Intent(AppActivity.this, ListActivity.class));
+            }
+        }, 1200); // 1sec
+    }
+
+    private void createPopupDialog() {
+
+        builder = new AlertDialog.Builder(AppActivity.this);
+        View view = getLayoutInflater().inflate(R.layout.popup, null);
+        babyItem = view.findViewById(R.id.babyItem);
+        itemQuantity = view.findViewById(R.id.itemQuantity);
+        itemColor = view.findViewById(R.id.itemColor);
+        itemSize = view.findViewById(R.id.itemSize);
+
+        saveButton = view.findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!babyItem.getText().toString().isEmpty()
+                        && !itemColor.getText().toString().isEmpty()
+                        && !itemQuantity.getText().toString().isEmpty()
+                        && !itemSize.getText().toString().isEmpty()){
+                    saveItem(view);
+                } else {
+                    Snackbar.make(view, "Empty Fields not Allowed", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        builder.setView(view);
+        dialog = builder.create();
+        dialog.show();
+
+    }
+
+}
---
+++ Project/BabyNeeds/app/src/main/java/com/paige/myapplication/data/DatabaseHandler.java
@@ -0,0 +1,154 @@
+package com.paige.myapplication.data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.paige.myapplication.model.Item;
+import com.paige.myapplication.util.Constants;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    public static final String TAG = "DBHandler";
+    private Context context;
+
+    public DatabaseHandler(@Nullable Context context) {
+        super(context, Constants.DB_NAME, null, Constants.DB_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_BABY_TABLE = "CREATE TABLE " + Constants.TABLE_NAME + "("
+                + Constants.KEY_ID + " INTEGER PRIMARY KEY,"
+                + Constants.KEY_BABY_ITEM + " INTEGER,"
+                + Constants.KEY_COLOR + " TEXT,"
+                + Constants.KEY_QTY_NUMBER + " TEXT,"
+                + Constants.KEY_ITEM_SIZE + " INTEGER,"
+                + Constants.KEY_DATE_NAME + " LONG);";
+
+        db.execSQL(CREATE_BABY_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_NAME);
+        onCreate(db);
+    }
+
+    // CRUD operations
+    public void addItem(Item item){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Constants.KEY_BABY_ITEM, item.getItemName());
+        contentValues.put(Constants.KEY_COLOR, item.getItemColor());
+        contentValues.put(Constants.KEY_QTY_NUMBER, item.getItemQuantity());
+        contentValues.put(Constants.KEY_ITEM_SIZE, item.getItemSize());
+        contentValues.put(Constants.KEY_DATE_NAME, System.currentTimeMillis());
+
+        //Insert the row
+        db.insert(Constants.TABLE_NAME, null, contentValues);
+
+        db.close();
+        Log.d(TAG, "added Item: " );
+    }
+
+    //Get an Item
+    public Item getItem(int id){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(
+                Constants.TABLE_NAME,
+                new String[]{Constants.KEY_ID, Constants.KEY_BABY_ITEM, Constants.KEY_QTY_NUMBER, Constants.KEY_ITEM_SIZE, Constants.KEY_DATE_NAME},
+                Constants.KEY_ID + " = ?",
+                new String[]{String.valueOf(id)},
+                null, null, null, null);
+        if(cursor != null)
+            cursor.moveToFirst();
+
+        Item item = new Item();
+        if(cursor != null) {
+            item.setId(Integer.parseInt(cursor.getString(cursor.getColumnIndex(Constants.KEY_ID))));
+            item.setItemName(cursor.getString(cursor.getColumnIndex(Constants.KEY_BABY_ITEM)));
+            item.setItemQuantity(cursor.getInt(cursor.getColumnIndex(Constants.KEY_QTY_NUMBER)));
+            item.setItemSize(cursor.getInt(cursor.getColumnIndex(Constants.KEY_ITEM_SIZE)));
+
+            //convert Timestamp to something readable
+            DateFormat dateFormat = DateFormat.getDateInstance();
+            String formattedDate = dateFormat.format(new Date(cursor.getLong(cursor.getColumnIndex(Constants.KEY_DATE_NAME))).getTime()); // Feb 23, 2020
+            item.setDateItemAdded(formattedDate);
+        }
+        cursor.close();
+        return item;
+    }
+
+    //Get all Items
+    public List<Item> getAllItems(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        List<Item> itemList = new ArrayList<>();
+        Cursor cursor = db.query
+                (Constants.TABLE_NAME,
+                new String[]{Constants.KEY_ID, Constants.KEY_BABY_ITEM, Constants.KEY_QTY_NUMBER, Constants.KEY_ITEM_SIZE, Constants.KEY_DATE_NAME},
+                null, null, null, null, Constants.KEY_DATE_NAME + " DESC", null);
+
+        if(cursor.moveToFirst()){
+            do {
+                Item item = new Item();
+                item.setId(Integer.parseInt(cursor.getString(cursor.getColumnIndex(Constants.KEY_ID))));
+                item.setItemName(cursor.getString(cursor.getColumnIndex(Constants.KEY_BABY_ITEM)));
+                item.setItemQuantity(cursor.getInt(cursor.getColumnIndex(Constants.KEY_QTY_NUMBER)));
+                item.setItemSize(cursor.getInt(cursor.getColumnIndex(Constants.KEY_ITEM_SIZE)));
+
+                //convert Timestamp to something readable
+                DateFormat dateFormat = DateFormat.getDateInstance();
+                String formattedDate = dateFormat.format(new Date(cursor.getLong(cursor.getColumnIndex(Constants.KEY_DATE_NAME))).getTime()); // Feb 23, 2020
+                item.setDateItemAdded(formattedDate);
+
+                itemList.add(item);
+            } while (cursor.moveToNext());
+        }
+        return itemList;
+    }
+
+    //Todo: Add updateItem
+    public int updateItem(Item item){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Constants.KEY_BABY_ITEM, item.getItemName());
+        contentValues.put(Constants.KEY_COLOR, item.getItemColor());
+        contentValues.put(Constants.KEY_QTY_NUMBER, item.getItemQuantity());
+        contentValues.put(Constants.KEY_ITEM_SIZE, item.getItemSize());
+        contentValues.put(Constants.KEY_DATE_NAME, System.currentTimeMillis());
+
+        int updated = db.update(Constants.TABLE_NAME,  contentValues,Constants.KEY_ID + " = ?", new String[]{String.valueOf(item.getId())});
+        db.close();
+        return updated;
+    }
+
+    //Todo: Add Delete Item
+    public void deleteItem(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(Constants.TABLE_NAME, Constants.KEY_ID + " = ?", new String[]{String.valueOf(id)});
+        db.close();
+    }
+
+    //Todo: getItemCount
+    public int getItemCount(){
+        String countQuery = "SELECT * FROM " + Constants.TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        int counts = cursor.getCount();
+        cursor.close();
+        return counts;
+    }
+
+}
