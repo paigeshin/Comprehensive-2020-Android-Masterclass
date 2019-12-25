2de4a1c443da482ed90dc82b8a6d101fa27f8690
shin <proscus0706@gmail.com>
2019-12-25 22:07:19 +0900

    구글맵

---
+++ Concept/GoogleMap/app/src/main/java/com/paige/googlemapandlocation/MainActivity.java
@@ -0,0 +1,104 @@
+package com.paige.googlemapandlocation;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private GoogleApiClient googleApiClient;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private ArrayList<String> permissionsToRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addOnConnectionFailedListener(this)
+                .addConnectionCallbacks(this)
+                .build();
+
+        fusedLocationProviderClient =  LocationServices.getFusedLocationProviderClient(this);
+
+
+    }
+
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        //Google API Test
+
+        int errorCode = GoogleApiAvailability.getInstance()
+                .isGooglePlayServicesAvailable(this);
+
+        if(errorCode != ConnectionResult.SUCCESS){
+            Dialog errorDialog = GoogleApiAvailability
+                                .getInstance()
+                                .getErrorDialog(this, errorCode, errorCode, new DialogInterface.OnCancelListener() {
+                                    @Override
+                                    public void onCancel(DialogInterface dialog) {
+                                        Toast.makeText(MainActivity.this, "Google Service Unavailable", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+            errorDialog.show();
+        } else {
+            Toast.makeText(MainActivity.this, "Google Service Available", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
